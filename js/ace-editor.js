4c1c24ecf236c7c082b3629126cea683f6a27918
nikotlichnik <shtreisnikita@gmail.com>
2018-04-18 02:29:33 +0300

    Созданы элементы интерфейса для демонстрации первого этапа курсовой

--- js/ace-editor.js
+++ js/ace-editor.js
@@ -1,21 +1,68 @@
-var codeEditor = document.querySelector(".task__editor");
-codeEditor.style.fontSize = '18px';
-
+// Iframe с результатом
 var iframe = document.querySelector(".task__result");
 
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/chrome");
-editor.session.setMode("ace/mode/javascript");
+
+// Редактор HTML
+var codeEditorHTML = document.querySelector(".task__editor--html");
+codeEditorHTML.style.fontSize = '18px';
+
+var editorHTML = ace.edit("editorHTML");
+editorHTML.setTheme("ace/theme/chrome");
+editorHTML.session.setMode("ace/mode/html");
+editorHTML.setValue("Здесь будет HTML");
+
+var iframeDocument = iframe.contentWindow.document.body;
+
+var onChangeHTML = function () {
+    iframeDocument.innerHTML = "";
+
+    var HtmlEditorContent = editorHTML.getValue();
+    iframeDocument.innerHTML = HtmlEditorContent;
+    console.log(HtmlEditorContent);
+};
+
+editorHTML.session.on('change', onChangeHTML);
+
+
+// Редактор JS
+var codeEditorJS = document.querySelector(".task__editor--javascript");
+codeEditorJS.style.fontSize = '18px';
+
+var editorJS = ace.edit("editorJS");
+editorJS.setTheme("ace/theme/chrome");
+editorJS.session.setMode("ace/mode/javascript");
+editorJS.setValue("Здесь будет JS");
 
 var userScript = document.createElement("script");
-var iframeDocument = iframe.contentWindow.document;
-iframeDocument.querySelector("body").appendChild(userScript);
+iframeDocument.appendChild(userScript);
 
-editor.session.on('change', function (delta) {
-    var editorContent = editor.getValue();
+var onChangeJS = function () {
+    var JsEditorContent = editorJS.getValue();
 
     var iframeScript = iframeDocument.querySelector("script");
-    iframeScript.textContent = editorContent;
-});
+    iframeScript.textContent = JsEditorContent;
+};
+
+editorJS.session.on('change', onChangeJS);
 
 
+// Редактор CSS
+var codeEditorCSS = document.querySelector(".task__editor--css");
+codeEditorCSS.style.fontSize = '18px';
+
+var editorCSS = ace.edit("editorCSS");
+editorCSS.setTheme("ace/theme/chrome");
+editorCSS.session.setMode("ace/mode/css");
+editorCSS.setValue("Здесь будет CSS");
+
+var userStyle = document.createElement("style");
+iframeDocument.appendChild(userStyle);
+
+var onChangeCSS = function () {
+    var CssEditorContent = editorCSS.getValue();
+
+    var iframeStyle = iframeDocument.querySelector("style");
+    iframeStyle.textContent = CssEditorContent;
+};
+
+editorCSS.session.on('change', onChangeCSS);
